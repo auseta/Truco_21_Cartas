10cf10c0b1ee5bc8ab91891b57d3b0cf6e1c55cc
augusto <augusto@192.168.0.11>
2023-06-24 18:22:47 -0300

    El proyecto fue creado. Se creó y configuró el archivo pom.xml

---
+++ src/main/java/main/Truco21Cartas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author augusto
+ */
+public class Truco21Cartas {
+
+    public static void main(String[] args) {
+        
+    }
+}
