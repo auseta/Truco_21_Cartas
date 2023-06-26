963c20e6d79ecbba18b523a2f8052beace0a1062
augusto <augusto@192.168.0.11>
2023-06-26 15:12:21 -0300

    Código agregado en el archivo .java Se importaron las clases Scanner y Random del paquete Util. Se creó un algoritmo que genera 21 caracteres de letras sin repetir.

--- src/main/java/main/Truco21Cartas.java
+++ src/main/java/main/Truco21Cartas.java
@@ -1,16 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package main;
 
-/**
- *
- * @author augusto
- */
+import java.util.Scanner;
+import java.util.Random;
+
 public class Truco21Cartas {
 
     public static void main(String[] args) {
+        var entrada= new Scanner(System.in);
+        var sorteador= new Random();
+        int i, j;
+        char[] arregloSinRepetir= new char[21];
+        char carta;
+        char[][] secuencia= new char[7][3];
+
+        //Generador de caracteres aleatorios sin repetir
+        for (i=0; i<21; i++) {
+            carta= (char)(sorteador.nextInt(85-65+1)+65);
+            j=0;
+            while(j<i){
+                if(arregloSinRepetir[j]==carta){
+                    carta= (char)(sorteador.nextInt(85-65+1)+65);
+                    j=0;
+                } else {
+                    j++;
+                }
+            }
+            arregloSinRepetir[i]=carta;
+        }
         
+        System.out.println("Haremos 3 secuencias. Empecemos...");
     }
 }
