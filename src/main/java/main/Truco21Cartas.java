30da17be1d807c6680af144b31150b5f7fbe8a87
augusto <augusto@192.168.0.11>
2023-06-28 01:58:31 -0300

    Logica del programa finalizada. Se creo un arreglo bidimensional ,el cual mediante un algoritmo, permite intercalar los grupos de arreglos los cuales fueron cambiados de lugar segun la eleccion de columna del usuario durante las secuencias. El programa adivina, sin margen de error, la letra que el usuario eligió.

--- src/main/java/main/Truco21Cartas.java
+++ src/main/java/main/Truco21Cartas.java
@@ -11,18 +11,22 @@ public class Truco21Cartas {
         final short MAX_TARJETAS= MAX_TARJETAS_GRUPO*MAX_GRUPOS; //Total de tarjetas.
         final short MIN_TARJETA_VALOR='A';
         final short MAX_TARJETA_VALOR= (char)(MAX_TARJETAS+(int)('A')-1); //Tarjeta final.
+        
         var generador= new Random();
         var entrada= new Scanner(System.in);
+        
         String grupoSeleccionado;
         char tarjeta;
-        int i,j,k,l;
-        
-        
-        char[] grupo1= new char[MAX_TARJETAS_GRUPO], grupo2= new char[MAX_TARJETAS_GRUPO], grupo3= new char[MAX_TARJETAS_GRUPO];
+        int i,j,k,l,m;
+           
+        char[] grupo1= new char[MAX_TARJETAS_GRUPO], 
+               grupo2= new char[MAX_TARJETAS_GRUPO], 
+               grupo3= new char[MAX_TARJETAS_GRUPO];
         char[] deck= new char[MAX_TARJETAS];
+        char[][] arregloIntercalado= new char[7][3];
         
         //Generar letras al azar:
-        for(i=0; i<MAX_TARJETAS; i++){
+        for(i=0; i<21; i++){
             j=0;
             tarjeta= (char)(generador.nextInt(MAX_TARJETA_VALOR-(int)(MIN_TARJETA_VALOR)+1)+(int)(MIN_TARJETA_VALOR));
             while(j<i){
@@ -40,9 +44,9 @@ public class Truco21Cartas {
         
         //Asignar cartas a las celdas de los grupos
         j=0;
-        for (i=0; i<MAX_GRUPOS; i++) {
+        for (i=0; i<3; i++) {
             k=0;
-            while(k<MAX_TARJETAS_GRUPO){
+            while(k<7){
                 switch (i) {
                     case 0:
                         grupo1[k]=deck[j];
@@ -64,40 +68,34 @@ public class Truco21Cartas {
                 }   
             }
         }
-        
-        //--- MOSTRANDO DATOS ---
-        System.out.print("Deck: ");
-        for(char card:deck){
-            System.out.print(card+" ");
+
+        j=0;
+        for(i=0; i<3; i++){
+            k=0;
+            while(k<7){
+                if(i==0){
+                    deck[j]=grupo2[k];
+                    k++;
+                    j++;
+                }
+                if(i==1){
+                    deck[j]=grupo1[k];
+                    k++;
+                    j++;
+                }
+                if(i==2){
+                    deck[j]=grupo3[k];
+                    k++;
+                    j++;
+                }
+            }
         }
-        System.out.println();
-        
-        System.out.print("Grupo 1: ");
-        for(char card:grupo1){
-            System.out.print(card+" ");
-        }
-        System.out.println();
-        
-        System.out.print("Grupo 2: ");
-        for(char card:grupo2){
-            System.out.print(card+" ");
-        }
-        System.out.println();
-        
-        System.out.print("Grupo 3: ");
-        for(char card:grupo3){
-            System.out.print(card+" ");
-        }
-        System.out.println();
-        //---FIN: MOSTRANDO DATOS ---
-        
-        var numero="3";
         
         System.out.println("Haremos 3 secuencias. Empecemos...");               
-        for(i=0; i<MAX_GRUPOS;i++){
+        for(i=0; i<3;i++){
             System.out.println("Secuencia "+(i+1)+" :");
             //--- IMPRIME LA SECUENCIA EN PANTALLA ---
-            for(j=0; j<MAX_TARJETAS_GRUPO;j++){
+            for(j=0; j<7;j++){
                 System.out.println("\t\t"+grupo1[j]+"   "+grupo2[j]+"   "+grupo3[j]);
                 System.out.println();
             }
@@ -108,6 +106,239 @@ public class Truco21Cartas {
                 System.out.print("ERROR - Opcion incorrecta, ingresa una opcion valida [1,2,3]: ");
                 grupoSeleccionado= entrada.nextLine();
             }
+            if(grupoSeleccionado.equals("1")){
+                k=0;
+                // --- CAMBIAR POSICION DEL GRUPO ---
+                for(l=0; l<3; l++){
+                    m=0;
+                    while(m<7){
+                        if(l==0){
+                            deck[k]=grupo2[m];
+                            m++;
+                            k++;
+                        }
+                        if(l==1){
+                            deck[k]=grupo1[m];
+                            m++;
+                            k++;
+                        }
+                        if(l==2){
+                            deck[k]=grupo3[m];
+                            m++;
+                            k++;
+                        }
+                    }
+                }
+                m=0;
+                for (l=0; l<3; l++) {
+                    k=0;
+                    while(k<7){
+                        switch (l) {
+                            case 0:
+                                grupo1[k]=deck[m];
+                                k++;
+                                m++;
+                                break;
+                            case 1:
+                                grupo2[k]=deck[m];
+                                k++;
+                                m++;
+                                break;
+                            case 2:
+                                grupo3[k]=deck[m];
+                                k++;
+                                m++;
+                                break;
+                            default:
+                                break;
+                        }   
+                    }
+                }
+                // --- RELLENAR ARREGLO BIDIMENSIONAL ---
+                m=0;
+                for(l=0; l<7;l++){
+                    k=0;
+                    while(k<3){
+                        arregloIntercalado[l][k]=deck[m];
+                        m++;
+                        k++;
+                    }
+                }
+                // --- RELLENAR GRUPOS INTERCALADOS ---
+                for(l=0; l<3;l++){
+                    k=0;
+                    while(k<7){
+                        switch (l) {
+                            case 0:
+                                grupo1[k]=arregloIntercalado[k][l];
+                                k++;
+                                break;
+                            case 1:
+                                grupo2[k]=arregloIntercalado[k][l];
+                                k++;
+                                break;
+                            default:
+                                grupo3[k]=arregloIntercalado[k][l];
+                                k++;
+                                break;
+                        }
+                    }
+                }
+            } else if (grupoSeleccionado.equals("2")){
+                k=0;
+                for(l=0; l<3; l++){
+                    m=0;
+                    while(m<7){
+                        if(l==0){
+                            deck[k]=grupo1[m];
+                            m++;
+                            k++;
+                        }
+                        if(l==1){
+                            deck[k]=grupo2[m];
+                            m++;
+                            k++;
+                        }
+                        if(l==2){
+                            deck[k]=grupo3[m];
+                            m++;
+                            k++;
+                        }
+                    }
+                }
+                m=0;
+                for (l=0; l<3; l++) {
+                    k=0;
+                    while(k<7){
+                        switch (l) {
+                            case 0:
+                                grupo1[k]=deck[m];
+                                k++;
+                                m++;
+                                break;
+                            case 1:
+                                grupo2[k]=deck[m];
+                                k++;
+                                m++;
+                                break;
+                            case 2:
+                                grupo3[k]=deck[m];
+                                k++;
+                                m++;
+                                break;
+                            default:
+                                break;
+                        }   
+                    }
+                }
+                // --- RELLENAR ARREGLO INTERCALADO ---
+                m=0;
+                for(l=0; l<7;l++){
+                    k=0;
+                    while(k<3){
+                        arregloIntercalado[l][k]=deck[m];
+                        m++;
+                        k++;
+                    }
+                }
+                // --- RELLENAR GRUPOS INTERCALADOS ---
+                for(l=0; l<3;l++){
+                    k=0;
+                    while(k<7){
+                        switch (l) {
+                            case 0:
+                                grupo1[k]=arregloIntercalado[k][l];
+                                k++;
+                                break;
+                            case 1:
+                                grupo2[k]=arregloIntercalado[k][l];
+                                k++;
+                                break;
+                            default:
+                                grupo3[k]=arregloIntercalado[k][l];
+                                k++;
+                                break;
+                        }
+                    }
+                }
+            } else{
+                k=0;
+                for(l=0; l<3; l++){
+                    m=0;
+                    while(m<7){
+                        if(l==0){
+                            deck[k]=grupo1[m];
+                            m++;
+                            k++;
+                        }
+                        if(l==1){
+                            deck[k]=grupo3[m];
+                            m++;
+                            k++;
+                        }
+                        if(l==2){
+                            deck[k]=grupo2[m];
+                            m++;
+                            k++;
+                        }
+                    }
+                }
+                m=0;
+                for (l=0; l<3; l++) {
+                    k=0;
+                    while(k<7){
+                        switch (l) {
+                            case 0:
+                                grupo1[k]=deck[m];
+                                k++;
+                                m++;
+                                break;
+                            case 1:
+                                grupo2[k]=deck[m];
+                                k++;
+                                m++;
+                                break;
+                            case 2:
+                                grupo3[k]=deck[m];
+                                k++;
+                                m++;
+                                break;
+                            default:
+                                break;
+                        }   
+                    }
+                }
+                m=0;
+                for(l=0; l<7;l++){
+                    k=0;
+                    while(k<3){
+                        arregloIntercalado[l][k]=deck[m];
+                        m++;
+                        k++;
+                    }
+                }
+                // --- RELLENAR GRUPOS INTERCALADOS ---
+                for(l=0; l<3;l++){
+                    k=0;
+                    while(k<7){
+                        switch (l) {
+                            case 0:
+                                grupo1[k]=arregloIntercalado[k][l];
+                                k++;
+                                break;
+                            case 1:
+                                grupo2[k]=arregloIntercalado[k][l];
+                                k++;
+                                break;
+                            default:
+                                grupo3[k]=arregloIntercalado[k][l];
+                                k++;
+                                break;
+                        }
+                    }
+                }
+            }
         }
+        System.out.println("Obviamente elegiste la "+ grupo2[3]);
     }
 }
